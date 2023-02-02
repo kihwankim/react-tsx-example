54618300a226b87276326ed6146172e52ba6a9ed
kihwankim <clack2933@gmail.com>
2023-02-02 22:48:10 +0900

    feat: type 외부로 분리 및 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import {TodoListItem} from "./TodoListItem";
 
-function App() {
-    return <div>Hello World</div>;
+const todos: Array<Todo> = [
+    {text: "walk the dog", complete: true},
+    {text: "write app", complete: false}
+]
+
+const App: React.FC = () => {
+    return <TodoListItem todo={todos[0]}/>;
 }
+
+// function App() {
+//     return <div>Hello World</div>;
+// }
 
 export default App;
