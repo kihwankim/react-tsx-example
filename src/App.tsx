d5f95c8af61aabae0aecda9f95afc07825e8fe47
kihwankim <clack2933@gmail.com>
2023-02-03 21:32:33 +0900

    feat: validation 로직 추가 완료

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ const App: React.FC = () => {
     }
 
     const addTodo: AddTodo = newTodo => {
-        setTodos([
+        newTodo.trim() !== "" && setTodos([
             ...todos,
             {text: newTodo, complete: false}
         ])
